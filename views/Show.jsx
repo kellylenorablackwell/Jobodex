8f360bd3fef11f67c64dffa08bffd2ff6f8c0a07
Kelly Blackwell <kellylenorablackwell@gmail.com>
2023-02-25 16:22:51 -0500

    Styled buttons on Show pagenodemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,9 +1,27 @@
 const React = require('react')
 const NavBar = require('./components/NavBar.jsx')
+const Button = require('./components/Button.jsx')
 
 class Show extends React.Component {
     render(){
         const {companyName, industry, jobTitle, salary, workLocation, jobSummary, educationAndExperience, uniqueBenefits, reasonForApplying, jobApplicationLink, _id} = this.props.job
+
+        const allFormsStyle = {
+            // border: '1px solid black',
+            display:'flex',
+        }
+
+        const buttonStyle = {
+            backgroundColor: '#004aad', 
+            color: 'white', 
+            fontFamily: 'Verdana',  
+            fontSize: '10px', 
+            borderStyle:'none',
+            borderRadius: '2px', 
+            width: '75px', 
+            height:'20px',
+        }
+
         return(
             <div>
                 <NavBar/>
@@ -16,15 +34,17 @@ class Show extends React.Component {
                 <h5>Education and Experience: {educationAndExperience}</h5>
                 <h5>If any, what unique benefits does the company offer?{uniqueBenefits}</h5>
                 <h5>What are your reasons for applying for this role?{reasonForApplying}</h5>
-                <form action={jobApplicationLink}>
-                    <input type="submit" value='Apply' />
-                </form>
-                <form action={`/jobs/${_id}/edit`}>
-                    <input type="submit" value='Edit' />
-                </form>
-                <form action={`/jobs/${_id}/?_method=DELETE`} method='POST'>
-                    <input type="submit" value='Delete' />
-                </form>
+                <div style={allFormsStyle}>
+                    <form action={jobApplicationLink} style={{paddingRight:'5px'}}>
+                        <input type="submit" value='Apply' style={buttonStyle}/>
+                    </form>
+                    <form action={`/jobs/${_id}/edit`} style={{paddingRight:'5px'}}>
+                        <input type="submit" value='Edit' style={buttonStyle}/>
+                    </form>
+                    <form action={`/jobs/${_id}/?_method=DELETE`} method='POST' style={{paddingRight:'5px'}}>
+                        <input type="submit" value='Delete' style={buttonStyle}/>
+                    </form> 
+                </div>
             </div>
         )
     }
