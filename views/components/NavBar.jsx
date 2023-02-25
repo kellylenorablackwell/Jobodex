424d090226429cfa8087086b8fd4bface9eb6440
Kelly Blackwell <kellylenorablackwell@gmail.com>
2023-02-24 21:55:42 -0500

    Styled NavBar component

--- views/components/NavBar.jsx
+++ views/components/NavBar.jsx
@@ -5,32 +5,41 @@ class NavBar extends React.Component {
         const ulStyle = {
           listStyle: 'none',
           display:'flex',
-          justifyContent: 'start',
+        //   justifyContent: 'start',
           marginTop:'1px',
-          marginLeft: '5px',
-          borderBottom: '1px solid gray'
+          marginLeft: '1px',
+          borderBottom: '1px solid lightGray',
+        //   border: '1px solid purple',
+          padding: '1px'
         }
 
         const logoStyle = {
             // border: '1px solid green',
-            paddingRight:'500px'
+            padding:'0px 600px 0px 0px',
         }
 
         const navBarStyle = {
             // border: '1px solid red',
             alignSelf:'center',
-            padding:'1px 25px 1px 25px',
-        
-          
+            padding:'0px 17px 0px 17px',
         }
-  
+
+        const aTagStyle = {
+            textDecoration: 'none',
+            fontFamily: 'Courier New',
+            fontSize: '10px',
+            color: 'black'
+            
+        } 
+        
         return(
             <div>
                 <ul style={ulStyle}>
-                    <li style={logoStyle}>{ <a href='/home'>{<img src="../logo.png" width='85' height='25' alt="" />}</a>}</li>
-                    <li style={navBarStyle}>{<a href='/jobs/new'>Add a Job</a>}</li>  
-                    <li style={navBarStyle}>{<a href='/jobs'>Saved Jobs</a>}</li>  
-                    <li style={navBarStyle}>{<a href='/exit'>Exit</a>}</li>  
+                    <li style={logoStyle}>{<a href='/home'>{<img src="../logo.png" width='85' height='25' alt="" />}</a>}</li>
+                    <li style={navBarStyle}>{<a style={aTagStyle} href='/home'>Home</a>}</li>  
+                    <li style={navBarStyle}>{<a style={aTagStyle} href='/jobs/new'>Add a Job</a>}</li>  
+                    <li style={navBarStyle}>{<a style={aTagStyle} href='/jobs'>Saved Jobs</a>}</li>  
+                    <li style={navBarStyle}>{<a style={aTagStyle} href='/exit'>Exit</a>}</li>  
                 </ul>
             </div>
 
